caa3e94af9a6bf60fe4eab82a08c989deb1c76e3
AWAIS <159710992+Awais-official-Dev@users.noreply.github.com>
2024-03-15 02:13:26 +0500

    Add files via upload

---
+++ day_03/main.ts
@@ -0,0 +1,17 @@
+// Day 3 Challenge: Start Coding! 🚀
+
+// Question 7: Number Eight: Write addition, subtraction, multiplication, and division operations that each result in the number 8. Enclose your operations in print statements.
+ console.log(4+4);
+ console.log(15-7);
+ console.log(4*2);
+ console.log(24/3);
+
+//  Question 9: Favorite Number: Store your favorite number in a variable. Then, create a message revealing your favorite number. Print that message.
+let favoriteNumber = 5;
+let message = "my favorite number is,"
+console.log(`${message}${favoriteNumber}`);
+
+ 
+ 
+ 
+ 
